5c3a7551f1fe3f18e33b2732419e0caf9e329dcd
vinaya-sv <vinayasv24@gmail.com>
2024-06-22 20:47:40 +0530

    Added sample login to the App.js

--- Frontend/room_to_read/App.js
+++ Frontend/room_to_read/App.js
@@ -1,12 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import LoginPage from './pages/LoginPage';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hey guys It's working alright!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <LoginPage/>
   );
 }
 
---
+++ Frontend/room_to_read/pages/LoginPage.js
@@ -0,0 +1,65 @@
+import React, { useState } from 'react';
+import { View, Text, TextInput, Button, StyleSheet, Alert } from 'react-native';
+
+const LoginPage = ({ navigation }) => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    // Here you would usually send the username and password to your backend server for verification
+    if (username === 'test' && password === '1234') {
+      Alert.alert('Login Successful');
+    } else {
+      Alert.alert('Login Failed', 'Invalid username or password');
+    }
+  };
+
+  const navigateToRegister = () => {
+    navigation.navigate('Register');
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Login</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Username"
+        value={username}
+        onChangeText={setUsername}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Password"
+        value={password}
+        onChangeText={setPassword}
+        secureTextEntry
+      />
+      <Button title="Login" onPress={handleLogin} />
+      <Text></Text>
+      <Button title="Register" onPress={navigateToRegister} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingHorizontal: 20,
+    backgroundColor: '#fff',
+  },
+  title: {
+    fontSize: 24,
+    marginBottom: 20,
+    textAlign: 'center',
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 20,
+    paddingHorizontal: 10,
+  },
+});
+
+export default LoginPage;
