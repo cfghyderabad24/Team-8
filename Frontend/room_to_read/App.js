41ce3fdb7b3cfa747dbd429a10e2109f75bac14f
AnuDeepthiGarlapati <anudeepthigarlapati@gmail.com>
2024-06-23 05:21:07 +0530

    Commiting App

--- Frontend/room_to_read/App.js
+++ Frontend/room_to_read/App.js
@@ -1,12 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { StyleSheet, View } from 'react-native';
+import TransactionPage from './pages/inout';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
+
 
 export default function App() {
+  const currPage="Sidebar";
   return (
-    <View style={styles.container}>
-      <Text>Hey guys It's working alright!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName={currPage} screenOptions={{headerShown: false }}>
+        <Stack.Screen name="Transcation" component={TransactionPage} />
+      </Stack.Navigator>
+    </NavigationContainer>
+
   );
 }
 
